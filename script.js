9b71a8629fdcbaa56aba562808d53f5534ac57a8
GabrielFlau132 <gabrielflaulhabe.mg@gmail.com>
2026-04-05 11:21:53 -0300

    botao de fechar e tratamento de async

--- script.js
+++ script.js
@@ -6,14 +6,24 @@ fetch("data.json")
     dados = data;
     document.getElementById("resumo").innerText =
       dados.biografia_resumida.texto;
+
+    document.getElementById("btn-saiba-mais").disabled = false; // ---
   });
 
 function abrirModal() {
-  if (!dados) return;
+  if (!dados) {
+    console.warn("dados nao carregados");
+    return;
+  }
 
   const modal = document.getElementById("modal");
   const texto = document.getElementById("texto-completo");
 
   texto.innerText = dados.biografia_completa.texto;
   modal.style.display = "block";
+}
+
+function fecharModal() {
+  // ---
+  document.getElementById("modal").style.display = "none";
 }
